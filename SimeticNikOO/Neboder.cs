b6845ae640c44112a260e13eecacaf9d54f549d9
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-03-10 11:44:39 +0100

    Add files via upload

---
+++ SimeticNikOO/Neboder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimeticNikOO
+{
+    class Neboder
+    {
+        private int visina;
+        private int BrojKatova;
+
+        public override string ToString()
+        {
+            string ispis = "\nVisina: " + visina + "\nBroj katova: " + BrojKatova;
+            return ispis;
+        }
+
+        public Neboder(int visina, int BrojKatova)
+        {
+            this.visina = visina;
+            this.BrojKatova = BrojKatova;
+        }
+
+
+
+
+        public int getVisina()
+        {
+            return visina;
+        }
+        public void setVisina(int visina)
+        {
+            this.visina = visina;
+        }
+        public int getBrojKatova()
+        {
+            return BrojKatova;
+        }
+        public void setBrojKatova(int BrojKatova)
+        {
+            this.BrojKatova = BrojKatova;
+        }
+
+
+    }
+}
+            
+    
+
---
+++ SimeticNikOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimeticNikOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float VisinaKataPr;
+
+            Neboder Tower = new Neboder(200, 100);
+
+            Tower.setVisina(Tower.getVisina() + 10);
+            Tower.setBrojKatova(Tower.getBrojKatova() + 1);
+
+            VisinaKataPr = Tower.getVisina() / Tower.getBrojKatova();
+            Console.WriteLine("Prosjecna visina kata je:"+ VisinaKataPr);
+            Console.ReadKey();
+        }
+    }
+}
